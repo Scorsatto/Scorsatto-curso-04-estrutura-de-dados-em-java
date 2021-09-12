5916d2e5a4688e0338f57a9fcaf5b7934ed6e493
Fabiano Scorsatto <fabianoscorsatto@gmail.com>
2021-09-12 05:14:42 -0400

    Explicação teórica do conceito de Filas

---
+++ filas-FIFO-first-in-first-out/src/main/java/com/projeto/No.java
@@ -0,0 +1,38 @@
+package com.projeto;
+
+public class No {
+
+    private Object object;
+    private No referenciaNo;
+
+    public No() {
+    }
+
+    public No(Object object) {
+        this.referenciaNo = null;
+        this.object = object;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+
+    public No getReferenciaNo() {
+        return referenciaNo;
+    }
+
+    public void setReferenciaNo(No referenciaNo) {
+        this.referenciaNo = referenciaNo;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" +
+                "object=" + object +
+                '}';
+    }
+}
